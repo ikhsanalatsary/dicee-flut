d5e04a0f3946ac6f9ef7438bcc213cc1a04cd493
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2019-02-17 20:16:09 +0700

    :tada: push all

---
+++ README.md
@@ -0,0 +1,22 @@
+# dicee_flut
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
+
+For help getting started with Flutter, view our 
+[online documentation](https://flutter.io/docs), which offers tutorials, 
+samples, guidance on mobile development, and a full API reference.
+
+## Version
+Flutter 1.0.0 stable version
+
+## Screenshot
+![Finished App](https://github.com/ikhsanalatsary/dicee-flut/blob/master/Screenshot.png)
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+      title: 'Welcome to Las Vegas',
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: ListView(
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 50.0),
+                  child: Image(
+                    image:AssetImage('assets/diceeLogo.png'),
+                  ),
+                ),
+              ),
+              RollDicee(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RollDiceeState extends State<RollDicee> {
+  String randomImage1 = getRandom();
+  String randomImage2 = getRandom();
+
+  static String getRandom() {
+    return (Random().nextInt(6) + 1).toString();
+  }
+
+  void rollingDicee(){
+    setState((){
+      randomImage1 = getRandom();
+      randomImage2 = getRandom();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isIPhone = Theme.of(context).platform == TargetPlatform.iOS;
+    return Container(
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 60.0, left: isIPhone ? 65.0 : 60.0, right: 10.0),
+                    child: Image(
+                      image: AssetImage('assets/dice' + randomImage1 + '.png'),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 60.0, left: isIPhone ? 65.0 : 60.0, right: 25.0),
+                    child: Image(
+                      image: AssetImage('assets/dice' + randomImage2 + '.png'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 70.0),
+              child: RaisedButton(
+                child: Text('ROLL'),
+                onPressed: rollingDicee,
+                padding: EdgeInsets.all(15.0),
+                color: Colors.redAccent[700],
+                textColor: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RollDicee extends StatefulWidget {
+  @override
+  RollDiceeState createState() => new RollDiceeState();
+}
