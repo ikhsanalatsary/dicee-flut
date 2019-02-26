6bde60a1ae23e86648cb3894e990fe3bec61ebe9
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2019-02-27 01:06:26 +0700

    :thumb_up: enable rotation and use private variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'dart:math';
 
 void main() => runApp(MyApp());
@@ -7,10 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
     return MaterialApp(
       title: 'Welcome to Las Vegas',
       home: Scaffold(
@@ -21,18 +16,31 @@ class MyApp extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          child: ListView(
-            children: <Widget>[
-              Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 50.0),
-                  child: Image(
-                    image: AssetImage('assets/diceeLogo.png'),
+          child: SingleChildScrollView(
+            child: IntrinsicHeight(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    // A fixed-height child.
+                    height: 180.0,
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 30.0),
+                        child: Image(
+                          image: AssetImage('assets/diceeLogo.png'),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  Expanded(
+                    // A flexible child that will grow to fit the viewport but
+                    // still be at least as big as necessary to fit its contents.
+                    flex: 1,
+                    child: RollDicee(),
+                  ),
+                ],
               ),
-              RollDicee(),
-            ],
+            ),
           ),
         ),
       ),
@@ -41,8 +49,8 @@ class MyApp extends StatelessWidget {
 }
 
 class RollDiceeState extends State<RollDicee> {
-  String randomImage1 = getRandom();
-  String randomImage2 = getRandom();
+  String _randomImage1 = getRandom();
+  String _randomImage2 = getRandom();
 
   static String getRandom() {
     return (Random().nextInt(6) + 1).toString();
@@ -50,52 +58,56 @@ class RollDiceeState extends State<RollDicee> {
 
   void rollingDicee() {
     setState(() {
-      randomImage1 = getRandom();
-      randomImage2 = getRandom();
+      _randomImage1 = getRandom();
+      _randomImage2 = getRandom();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     bool isIPhone = Theme.of(context).platform == TargetPlatform.iOS;
+    // double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    bool isPotrait = MediaQuery.of(context).orientation == Orientation.portrait;
     return Container(
+      padding: EdgeInsets.only(top: isPotrait ? 50.0 : 10.0),
+      height: isPotrait ? height * (isIPhone ? 0.8 : 0.7) : (isIPhone ? height / 1.5 : height / 2),
       child: Center(
         child: Column(
           children: <Widget>[
             Container(
-              // color: Colors.yellow,
-              margin: EdgeInsets.only(
-                  left: isIPhone ? 70.0 : 60.0,
-                  right: isIPhone ? 70.0 : 60.0,
-                  top: isIPhone ? 80.0 : 40.0,
-                  bottom: 20.0),
+              margin: EdgeInsets.only(bottom: isPotrait ? 20.0 : 10.0),
               child: Row(
+                mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Padding(
                     padding: EdgeInsets.only(right: 10.0),
                     child: Image(
-                      image: AssetImage('assets/dice' + randomImage1 + '.png'),
+                      image: AssetImage('assets/dice$_randomImage1.png'),
                     ),
                   ),
                   Padding(
                     padding: EdgeInsets.only(left: 10.0),
                     child: Image(
-                      image: AssetImage('assets/dice' + randomImage2 + '.png'),
+                      image: AssetImage('assets/dice$_randomImage2.png'),
                     ),
                   ),
                 ],
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 70.0),
-              child: RaisedButton(
-                child: Text('ROLL'),
-                onPressed: rollingDicee,
-                padding: EdgeInsets.all(15.0),
-                color: Colors.redAccent[700],
-                textColor: Colors.white,
+            Flexible(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.only(top: isPotrait ? 70.0 : 20.0),
+                child: RaisedButton(
+                  child: Text('ROLL'),
+                  onPressed: rollingDicee,
+                  padding: EdgeInsets.all(15.0),
+                  color: Colors.redAccent[700],
+                  textColor: Colors.white,
+                ),
               ),
             ),
           ],
